03c86402b0751b5480db6a4b43b05c4fc87e4e82
Andres Diago <andresdiag@unicauca.edu.co>
2023-01-29 10:45:37 -0500

    PRIMER COMMIT

---
+++ proyecto/routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/**
+ * Route:get    | Consultar
+ * Route:post   | Guardar
+ * Route:delete | Eliminar
+ * Route:put    | Actualizar
+ */
+
+Route::get('/', function () {
+    //return view('welcome');
+    return view('home');
+});
+
+Route::get('blog', function () {
+    //return view('welcome');
+
+    //Consulta a base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP',     'slug' => 'php'],
+        ['id' => 2, 'title' => 'LARAVEL', 'slug' => 'laravel'],
+        ['id' => 3, 'title' => 'JS',      'slug' => 'js'],
+    ];
+
+    return view('blog', ['posts' => $posts]);
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    //Consulta a base de datos
+    $post = $slug;
+
+    return view('post', ['post' => $post]);
+});
+
+Route::get('buscar', function (Request $request) {
+    //Consulta a base de datos
+    return $request->all();
+});
