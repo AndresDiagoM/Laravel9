73df2aace0ee665655305242d32377b0e217dd7e
Andres Diago <andresdiag@unicauca.edu.co>
2023-03-06 14:30:34 -0500

    :sparkles: Validación de registros duplicados
    se añade la validación tanto en la creación, como en la edición.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,16 +27,18 @@ class PostController extends Controller
         //validar los datos
         $request->validate([
             'title'=>'required',
+            'slug'=>'required|unique:posts,slug', //para que no se repita el slug
             'body'=>'required',
         ],[ //para mostrar un mensaje de error en español
             'title.required'=>'Este campo es requerido',
+            'slug.required'=>'Este campo es requerido',
             'body.required'=>'Se necesita mínimo un párrafo',
         ]);
         //si se envia el formulario sin datos, para mostrar un mensaje es necesario ediar el archivo de la vista _form.blade.php
 
         $post = $request->user()->posts()->create([
-            'title' => $title = $request->title,
-            'slug' => Str::slug($title),
+            'title' =>$request->title,
+            'slug' => $request->slug,
             'body' => $request->body,
         ]);
         //es con user porque el post pertenece a un usuario
@@ -57,9 +59,16 @@ class PostController extends Controller
     //vista update 
     public function update(Request $request, Post $post){
 
+        //validar los datos
+        $request->validate([
+            'title'=>'required',
+            'slug'=>'required|unique:posts,slug,' . $post->id, //para que haga la validación, pero sin tener en cuenta el mismo registro, para evitar este error
+            'body'=>'required',
+        ]);
+
         $post->update([
-            'title' => $title = $request->title,
-            'slug' => Str::slug($title),
+            'title' => $request->title,
+            'slug' => $request->slug,
             'body' => $request->body,
         ]);
         //Funcion de editar post
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -9,6 +9,14 @@
 <input type="text" name="title" class="rounded text-black border-gray-200 w-full mb-4" value="{{ old('title', $post->title) }}"> 
 {{-- old('title', $post->title) es para que no se borre el campo cuando se envia el formulario y hay un error en el mismo. --}}
 
+<label class="uppercase text-white-700 text-xs"> Slug </label>
+<span class="text-red-500 text-xs"> 
+    @error('slug') 
+        {{ $message }}
+    @enderror
+</span>
+<input type="text" name="slug" class="rounded text-black border-gray-200 w-full mb-4" value="{{ old('slug', $post->slug) }}"> 
+
 <label class="uppercase text-white-700 text-xs"> Contenido </label>
 <span class="text-red-500 text-xs"> 
     @error('body') 
