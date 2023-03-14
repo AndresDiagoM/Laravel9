05cf9e756dfad789e73d4ee3713333481aeb26d1
Andres Diago <andresdiag@unicauca.edu.co>
2023-03-13 23:13:26 -0500

    :boom: Se empieza a trabajar con el css.
    Se añade la configuración en app.js, y se configura el template con la imagen.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,23 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto Web</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <p>
-        <a href="{{ route('home') }}">HOME</a>
-        <a href="{{ route('blog') }}">BLOG</a>
 
-        @auth
-            <a href="{{ route('dashboard') }}">Dashboard</a>
-        @else
-            <a href="{{ route('login') }}">LOGIN</a>
-            <a href="{{ route('register') }}">REGISTRO</a>
-        @endauth
-    </p>
+    {{-- CONTENEDOR DEL MENU SUPERIOR Y PÁGINA --}}
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex item-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/chip.png') }}" alt="Logo" class="h-12">
+                </a>
 
-    <hr>
+                <form action="{{ route('blog') }}" method="GET">
+                    <input type="text" name="search" placeholder="Buscar...">
+                </form>
+            </div>
 
-    @yield('content')
+            @auth
+                <a href="{{ route('dashboard') }}">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}">LOGIN</a>
+                <a href="{{ route('register') }}">REGISTRO</a>
+            @endauth
+
+        </header>
+
+        @yield('content')
+    </div>
 
 </body>
 </html>
